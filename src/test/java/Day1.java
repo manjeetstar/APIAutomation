a467e87ac586f77132f82e2a9f183db9d00c3a62
Manjeet Singh <manjeetikon@gmail.com>
2026-02-07 09:54:19 +0530

    Added restassured project structure and prerequisites

---
+++ src/test/java/Day1.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class Day1 {
+    @Test
+    public void Test1() {
+        System.out.println("This is the first sample test");
+    }
+
+    @Test
+    public void Test2() {
+        System.out.println("This is the second sample test");
+    }
+}
