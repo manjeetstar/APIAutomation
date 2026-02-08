67465dcdfe25c73a2622addeddf325ea2a607e5d
Manjeet Singh <manjeetikon@gmail.com>
2026-02-08 12:23:54 +0530

    Added various headers

--- src/test/java/Day1.java
+++ src/test/java/Day1.java
@@ -1,9 +1,12 @@
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
-import io.restassured.response.Response;
+
+import java.util.Map;
+import io.restassured.response.Response; 
 
 public class Day1 {
+    public Long PetID;
     @BeforeClass
     public void setup() {
         baseURI = "https://petstore.swagger.io";
@@ -12,19 +15,49 @@ public class Day1 {
     @Test
     public void Test1() {
         Response r1= given()
-           .pathParam("petId", 203192)
+           .headers(Map.of("Content-Type","application/JSON", "Accept", "application/json"))
+           .auth().none()
+           .relaxedHTTPSValidation()      
+           .body("""
+                {
+                "id": 0,
+                "category": {
+                    "id": 0,
+                    "name": "string"
+                },
+                "name": "Apple Desktop",
+                "photoUrls": [
+                    "string"
+                ],
+                "tags": [
+                    {
+                    "id": 0,
+                    "name": "string"
+                    }
+                ],
+                "status": "available"
+                }
+            """)                              
         .when()
-            .get("/pet/{petId}")
+            .post("/pet")
         .then()
             .statusCode(200)
             .extract()
             .response(); 
             
-        System.out.println(r1.prettyPrint());
+    PetID= r1.jsonPath().getLong("id");   
     }
 
-    @Test
+    @Test(enabled = true)
     public void Test2() {
-        System.out.println("This is the second sample test");
+        given()
+           .headers(Map.of("Content-Type","application/JSON", "Accept", "application/json"))          
+           .pathParam("petId", PetID)                              
+        .when()
+            .get("/pet/{petId}")
+        .then()            
+            .statusCode(200)
+            .extract()
+            .response();             
     }
 }
