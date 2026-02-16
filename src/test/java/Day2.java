ec0c0dc1c440c3ebbd7ab3e487e2bb3b1e079652
Manjeet Singh <manjeetikon@gmail.com>
2026-02-16 09:19:57 +0530

    Updated dependency package

--- src/test/java/Day2.java
+++ src/test/java/Day2.java
@@ -6,20 +6,19 @@ import DataClass.ReceipeResponseDetails;
 import io.restassured.response.Response;
 
 @SuppressWarnings("unused")
-public class Day2 extends Day1{
-   
-   @Test
-   public void addReceipe(){
-    Response res=given()
-								.spec(reqSpec)								
-							.when()
-								.get("/recipes")
-							.then()
-								.statusCode(200)
-								.extract()
-								.response();
+public class Day2 extends Day1 {
 
-	System.out.println("Meal Type is "+ res.jsonPath().getList("recipes.findAll{it.difficulty == 'Easy'}").size());
-	//System.out.println("ID is "+ res.jsonPath().getInt("id"));
-   }
+	@Test
+	public void addReceipe() {
+		Response res = given()
+					.spec(reqSpec)
+				.when()
+					.get("/recipes")
+				.then()
+					.statusCode(200)
+					.extract()
+					.response();
+
+		System.out.println("Meal Type is " + res.jsonPath().getList("recipes.findAll{it.difficulty == 'Easy'}").size());
+	}
 }
